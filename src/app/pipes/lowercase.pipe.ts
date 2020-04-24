3f395db0c92bb91a370c1d82162e98dd52d892ef
laurenzfiala <laurenz.fiala@outlook.at>
2020-04-24 13:47:26 +0200

    * 2fa: fix spelling mistake in german translation

--- src/app/pipes/lowercase.pipe.ts
+++ src/app/pipes/lowercase.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {StringModificationPipe, StringModificationType} from "./strmod.pipe";
+import {StringModificationPipe, StringModificationType} from './strmod.pipe';
 
 @Pipe({name: 'lowercase'})
 export class LowercasePipe implements PipeTransform {
