08a611b0b94b0d8c15c2f837bdfba839a38bed12
laurenzfiala <laurenz.fiala@outlook.at>
2020-12-14 15:40:34 +0100

    ~ rename all diary refs to journal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -109,7 +109,7 @@ export const environment = {
     roles: {
       user:                             ['ROLE_USER'],
       treeEditor:                       ['ROLE_TREE_EDITOR'],
-      diary:                            ['ROLE_DIARY'],
+      journal:                          ['ROLE_JOURNAL'],
       phenObs:                          ['ROLE_PHENOBS'],
       admin:                            ['ROLE_ADMIN'],
       tempChangePassword:               'ROLE_TEMP_CHANGE_PASSWORD'
